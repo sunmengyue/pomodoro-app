9ea02d089563780c1b5401a01518dd3bd85ca7e0
bardrabbit <bardrabbit@gmail.com>
2021-06-21 22:13:24 -0400

    connect class functions to frontend

---
+++ js/index.js
@@ -0,0 +1,28 @@
+const timer = new Timer();
+timer.reset();
+
+const action = (action = "stop") => {
+    switch(action.toLowerCase()) {
+        case 'start':
+            timer.start()
+            break;
+        default:
+            timer.stop();
+            break;
+    }
+}
+
+// navigation
+const navLinks = document.querySelectorAll('.nav > ul > li');
+const navBg = document.getElementById('indicator');
+
+navLinks.forEach((navItem, i) => {
+    navItem.addEventListener('click', () => {
+        navLinks.forEach((link) => {
+            link.classList.remove('active');
+            navBg.style.marginLeft = `calc(calc(100% / 3) * ${i})`
+            navItem.classList.add('active');
+            
+        })
+    })
+});
--- js/timer.js
+++ js/timer.js
@@ -2,59 +2,77 @@ class Timer {
   constructor(type = 'pomodoro') {
     this.type = type;
     this.pomodoro = 25;
-    this.shortBreak = 5;
-    this.longBreak = 15;
-    this.timeElement = window[type];
+    this.shortbreak = 5;
+    this.longbreak = 15;
+   
     this.clock = document.getElementById('time');
     this.actionElement = document.getElementById('action');
     this.time = this.timeElement;
     this.text = this.time <= 9 ? `0${this.time}` : `${this.time}`;
     this.circle = document.querySelector('#ring > circle');
-    this.interval = 0;
-    this.clock = document.getElementById('clock');
-    this.control = document.getElementById('control');
+    // this.interval = 0;
+    // this.clock = document.getElementById('clock');
+    // this.control = document.getElementById('control');
   }
 
   stop() {
     clearInterval(this.interval);
+    this.actionElement.innerText = 'start';
   }
 
   select(type) {
     this.type = type;
-    switch (type) {
-      case 'shortbreak':
-        return (this.clock.innerText = 5);
-      case 'longbreak':
-        return (this.clock.innerText = 15);
-      default:
-        return (this.clock.innerText = 25);
-    }
+    this.reset();
   }
 
   start() {
-    let timeLeft = this.pomodoro;
-    if (this.type == 'shortbreak') {
-      timeLeft = this.shortBreak;
-    } else if (this.type == 'longbreak') {
-      timeLeft = this.longBreak;
-    }
+    const format = (time) => (time < 10 ? "0" + time : time) 
+    let time = this[this.type] * 60;
+    this.clock.innerText = `${this.text}:00`;
+    this.circle.style.strokeDashoffset = 1024;
+    
+    let startTime = time;
+    let minutes = 0;
+    let seconds = 0;
+
+    time--;
     this.interval = setInterval(() => {
-      timeLeft--;
-      this.clock.innerText = timeLeft;
-      this.control.innerText = 'stop';
-      if (timeLeft <= 0) clearInterval(this.interval);
-    }, 1000);
+      minutes = Math.floor(time / 60);
+      seconds = Math.floor(time % 60);
+     
+      minutes = format(minutes);
+      seconds = format(seconds);
+
+      this.clock.innerText = `${minutes}:${seconds}`;
+      const percent = ((time % startTime)/startTime) * 100;
+      const offset = (percent / 100) * 1024;
+      this.circle.style.strokeDashoffset = offset;
+
+      if(--time < 0) {
+        time = 0;
+        clearInterval(this.interval);
+        this.actionElement.innerText = 'reset';
+      }
+
+    }, 1000)
+
+    this.actionElement.innerText = 'stop';
   }
 
   reset() {
     this.stop();
-    switch (this.type) {
-      case 'shortbreak':
-        return (this.clock.innerText = 5);
-      case 'longbreak':
-        return (this.clock.innerText = 15);
-      default:
-        return (this.clock.innerText = 25);
-    }
+    this.circle.style.strokeDashoffset = 1024;
+    this.time = this[this.type];
+    this.text = this.time <= 9 ? `0${this.time}` : `${this.time}`;
+    this.actionElement.innerText = "start";
+    this.clock.innerText = `${this.text}:00`;
+    // switch (this.type) {
+    //   case 'shortbreak':
+    //     return (this.clock.innerText = 5);
+    //   case 'longbreak':
+    //     return (this.clock.innerText = 15);
+    //   default:
+    //     return (this.clock.innerText = 25);
+    // }
   }
 }
